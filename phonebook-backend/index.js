29489833d14f61905c9d3acbbd7bb36bba1e1c53
zntb <zenetibi@yahoo.com>
2024-06-12 16:16:20 +0300

    Add finished exercise 3.17

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -9,7 +9,9 @@ app.use(cors());
 app.use(express.json());
 
 morgan.token('body', (request, response) =>
-  request.method === 'POST' ? JSON.stringify(request.body) : ''
+  request.method === 'POST' || request.method === 'PUT'
+    ? JSON.stringify(request.body)
+    : ''
 );
 
 app.use(
@@ -80,6 +82,24 @@ app.post('/api/persons', (request, response, next) => {
     .save()
     .then((savedPerson) => {
       response.json(savedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { number } = request.body;
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        response.json(updatedPerson);
+      } else {
+        response.status(404).send({ error: 'person not found' });
+      }
     })
     .catch((error) => next(error));
 });
