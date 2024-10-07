845fb4ede464e246a240c91d00abd4a0c38ab25b
zntb <zenetibi@yahoo.com>
2024-10-07 09:56:54 +0300

    Update index.js - add healtz route

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const Person = require('./models/person');
 app.use(cors());
 app.use(express.json());
 
-morgan.token('body', (request) =>
+morgan.token('body', request =>
   request.method === 'POST' || request.method === 'PUT'
     ? JSON.stringify(request.body)
-    : ''
+    : '',
 );
 
 app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'),
 );
 
 app.use(express.static('dist'));
@@ -40,32 +40,36 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello Express!</h1>');
 });
 
+app.get('/healthz', (req, res) => {
+  res.send('ok');
+});
+
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then((persons) => {
+  Person.find({}).then(persons => {
     response.json(persons);
   });
 });
 
 app.get('/info', (request, response, next) => {
   Person.countDocuments({})
-    .then((count) => {
+    .then(count => {
       response.send(
-        `<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`
+        `<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`,
       );
     })
-    .catch((error) => next(error));
+    .catch(error => next(error));
 });
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-    .then((person) => {
+    .then(person => {
       if (person) {
         response.json(person);
       } else {
         response.status(404).send({ error: 'person not found' });
       }
     })
-    .catch((error) => next(error));
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response, next) => {
@@ -84,10 +88,10 @@ app.post('/api/persons', (request, response, next) => {
 
   person
     .save()
-    .then((savedPerson) => {
+    .then(savedPerson => {
       response.json(savedPerson);
     })
-    .catch((error) => next(error));
+    .catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -96,36 +100,37 @@ app.put('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndUpdate(
     request.params.id,
     { name, number },
-    { new: true, runValidators: true, context: 'query' }
+    { new: true, runValidators: true, context: 'query' },
   )
-    .then((updatedPerson) => {
+    .then(updatedPerson => {
       if (updatedPerson) {
         response.json(updatedPerson);
       } else {
         response.status(404).send({ error: 'person not found' });
       }
     })
-    .catch((error) => next(error));
+    .catch(error => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
   const personId = request.params.id;
 
   Person.findByIdAndDelete(personId)
-    .then((person) => {
+    .then(person => {
       if (person) {
         response.status(204).end();
       } else {
         response.status(404).send({ error: 'person not found' });
       }
     })
-    .catch((error) => next(error));
+    .catch(error => next(error));
 });
 
 app.use(unknownEndpoint);
 
 app.use(errorHandler);
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
